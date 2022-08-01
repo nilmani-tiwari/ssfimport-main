5efed7a727203a071b76d9c09498a8f045718957
nilmani-tiwari <sambadnilmani085@gmail.com>
2022-08-01 14:46:16 +0530

    ssf edit according to country

--- ssfimport/settings.py
+++ ssfimport/settings.py
@@ -146,7 +146,7 @@ WSGI_APPLICATION = 'ssfimport.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '../newssf-db/db.sqlite3',
+        'NAME': BASE_DIR / '../ssf-db/db.sqlite3',
     }
 }
 
